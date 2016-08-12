edabe9a32478a6b40b40192ad377aa73203a3388
user <regan_32@mail.ru>
2016-08-12 08:49:43 +0300

    ubuntu clion

--- Wahrscheinlichkeitstheorie/Wahrscheinlichkeitstheorie.cpp
+++ Wahrscheinlichkeitstheorie/Wahrscheinlichkeitstheorie.cpp
@@ -1,12 +1,30 @@
 // Wahrscheinlichkeitstheorie.cpp : Defines the entry point for the console application.
 //
 
+#ifdef _WIN32
 #include "stdafx.h"
+#endif
+
 #include <unordered_map>
+#include <iostream>
+#include <functional>
+#include <vector>
+
 //HOLDEM
 const size_t total_cards = 52;
 const size_t total_combinations = 380204032; // 52^5;
 
+enum HandMultiplyer
+{
+    Best = 1,
+    Pair = 10,
+    Triple = 100,
+    Street = 1000,
+    FullHouse = 10000,
+    Flash = 100000,
+    Quad = 1000000,
+    StreetFlash = 10000000
+};
 struct DeckImpl
 {
     DeckImpl()
@@ -44,16 +62,38 @@ struct FullDeck : public DeckImpl
     }
 };
 
-bool Calc_matches(unsigned long long hand, int matches)
+class HoldemPower
 {
-    int result = 0;
-    for(int i = 4; i > 0; --i)
+    public:
+        static unsigned long long Calculate_Power(DeckImpl hand)
+        {
+            m_functions.push_back([](DeckImpl hand){ return 0;});
+        }
+
+private:
+    static std::vector<std::function<unsigned long long(DeckImpl)>> m_functions;
+
+    unsigned long long IsQuad(DeckImpl hand)
     {
-        result += (hand >> i) & 0x1;
+        unsigned single =0;
+        for (int i = 0; i < 13; ++i)
+        {
+            if(Calc_matches(hand.mask >> (i * 4), 4))
+                return (i+1) * HandMultiplyer::Quad;
+        }
     }
-    
-    return result == matches;
-}
+    bool Calc_matches(unsigned long long hand, int matches)
+    {
+        int result = 0;
+        for(int i = 4; i > 0; --i)
+        {
+            result += (hand >> i) & 0x1;
+        }
+
+        return result == matches;
+    }
+};
+
 
 void CheckCombination(size_t cards, size_t positions,unsigned long long hand, std::unordered_map<unsigned long long, int>& hash_table)
 {
@@ -64,7 +104,8 @@ void CheckCombination(size_t cards, size_t positions,unsigned long long hand, st
         if (cards_left > 0 && i > 0)
             CheckCombination(cards_left, i - 1, hand | 1ULL << i, hash_table);
         else if(cards_left == 0)
-            hash_table.insert_or_assign(hand | 1ULL << i, 1);
+            hash_table.insert(std::make_pair(hand | 1ULL << i, 1));
+            //hash_table.insert_or_assign(hand | 1ULL << i, 1);
     }
 }
 
@@ -72,6 +113,7 @@ void Init_Combinations(size_t cards)
 {
     std::unordered_map<unsigned long long, int> hash_table;
     CheckCombination(cards, total_cards, 0ULL, hash_table);
+    std::cout<< hash_table.size();
 }
 
 unsigned long long CalcDeckPower(DeckImpl hand)
